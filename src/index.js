f6a2e1ce073589e90fb8159bd1962901f03cc3b4
Anna Półtorak <aniajtp@gmail.com>
2019-02-19 23:09:37 +0100

    add files

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import DevTools from './DevTools';
+
+import App from './App.js';
+import * as serviceWorker from './serviceWorker.js';
+import { reducer } from './reducer.js';
+import {
+  addComment
+} from './actions.js';
+
+import './index.css';
+
+
+
+const store = createStore(
+  reducer,
+  DevTools.instrument()
+);
+
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
